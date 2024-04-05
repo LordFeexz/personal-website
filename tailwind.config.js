a0ae77fe7fe21e74d53e5fd93d2c712c473af331
anandafiqri <anandafiqri1403@gmail.com>
2024-04-05 10:41:43 +0700

    try change tailwind config from ts to js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
-import type { Config } from "tailwindcss";
 const WithMt = require("@material-tailwind/react/utils/withMt");
 
-const config: Config = {
+const config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
