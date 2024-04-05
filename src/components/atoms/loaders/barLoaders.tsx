ac46b11ba304570d00f20399a70fb57214e4b06b
anandafiqri <anandafiqri1403@gmail.com>
2024-04-05 13:17:28 +0700

    remove bar loader

--- src/components/atoms/loaders/barLoaders.tsx
+++
@@ -1,9 +0,0 @@
-import { Spinner } from "./material-tailwind";
-
-export default function BarLoader() {
-  return (
-    <div className="flex justify-center items-center">
-      <Spinner className="h-6 w-6" />
-    </div>
-  );
-}
