59c9ee6ba1993608c70fcef4dd8663c6512ef172
anandafiqri <anandafiqri1403@gmail.com>
2024-08-05 16:51:24 +0700

    update form component

--- src/components/molleculs/form/contactForm.tsx
+++ src/components/molleculs/form/contactForm.tsx
@@ -4,6 +4,9 @@ import { useState, useEffect, type ChangeEvent, Suspense } from "react";
 import { useSearchParams } from "next/navigation";
 import { useFormStatus } from "react-dom";
 import { swalError, swalSuccess } from "@/libs/swal";
+import { cn } from "@/libs/utils";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
 
 export interface ContactFormProps {
   email: string;
@@ -57,24 +60,28 @@ export default function ContactForm() {
   return (
     <Suspense>
       <div className="flex w-full flex-col space-y-4 md:flex-row md:space-x-2 md:space-y-0">
-        <div className="w-full font-sora text-xl space-y-2">
-          <input
+        <LabelInputContainer className="w-full font-sora text-xl space-y-2">
+          <Label htmlFor="name">name</Label>
+          <Input
             value={data.name}
+            id="name"
             name="name"
             onChange={onChangeHandler}
             placeholder="name"
             className="w-full rounded-lg bg-neutral-50 p-2 outline outline-neutral-300 focus:outline-neutral-400 dark:bg-neutral-900 dark:outline-neutral-700"
           />
-        </div>
-        <div className="w-full text-xl font-sora space-y-2">
-          <input
+        </LabelInputContainer>
+        <LabelInputContainer className="w-full text-xl font-sora space-y-2">
+          <Label htmlFor="email">email</Label>
+          <Input
+            id="email"
             value={data.email}
             name="email"
             onChange={onChangeHandler}
             placeholder="email"
             className="w-full rounded-lg bg-neutral-50 p-2 outline outline-neutral-300 focus:outline-neutral-400 dark:bg-neutral-900 dark:outline-neutral-700"
           />
-        </div>
+        </LabelInputContainer>
       </div>
       <textarea
         color="blue"
@@ -97,3 +104,17 @@ export default function ContactForm() {
     </Suspense>
   );
 }
+
+const LabelInputContainer = ({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) => {
+  return (
+    <div className={cn("flex flex-col space-y-2 w-full", className)}>
+      {children}
+    </div>
+  );
+};
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,88 +1,97 @@
 import type { Config } from "tailwindcss";
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
 
-const config: Config = {
+const config = {
+  darkMode: ["class"],
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
   ],
+  prefix: "",
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
       colors: {
-        darkText: "#E4E6EB",
-        dark: "#121212",
-        light: "#fafafa",
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
       },
-      fontFamily: {
-        "roboto-condensed": ["var(--robotoCondensed-fon)"],
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
       },
       keyframes: {
-        wave: {
-          "0%": { transform: "rotate(0.0deg)" },
-          "10%": { transform: "rotate(14deg)" },
-          "20%": { transform: "rotate(-8deg)" },
-          "30%": { transform: "rotate(14deg)" },
-          "40%": { transform: "rotate(-4deg)" },
-          "50%": { transform: "rotate(10.0deg)" },
-          "60%": { transform: "rotate(0.0deg)" },
-          "100%": { transform: "rotate(0.0deg)" },
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
         },
-        flying: {
-          "0%": { transform: "translateY(0)" },
-          "50%": { transform: "translateY(0.8rem)" },
-          "100%": { transform: "translateY(0)" },
-        },
-        badge: {
-          "100%": {
-            transform: "scaleY(1.7) scaleX(1.25)",
-            opacity: "0",
-          },
-        },
-        slide: {
-          "0%": { transform: "translateX(0)" },
-          "20%": { transform: "translateX(-100%)" },
-          "50%": { transform: "translateX(-100%)" },
-          "90%": { transform: "translateX(5%)" },
-          "100%": { transform: "translateX(0)" },
-        },
-        slideInfinite: {
-          "0%": { transform: "translateX(0)" },
-          "100%": { transform: "translateX(-300%)" },
-        },
-        rainArrow: {
-          "0%": { transform: "translateY(-10%)" },
-          "50%": { transform: "translateY(10%)" },
-          "100%": { transform: "translateY(-10%)" },
-        },
-        enterLeft: {
-          "0%": { transform: "translateX(100px)", opacity: "0" },
-          "100%": { transform: "translateX(0)", opacity: "1" },
-        },
-        rightInfinite: {
-          "0%": { transform: "translateX(-150px)" },
-          "100%": { transform: "translateX(150px)" },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
         },
       },
       animation: {
-        "waving-hand": "wave 2s linear infinite",
-        "flying-card": "flying 3s infinite normal",
-        "badge-pulse": "badge 1.5s ease-out infinite",
-        "slide-card": "slide 3s 3s ease-in-out",
-        "slide-infinite": "slideInfinite 100s linear infinite",
-        "rain-arrow": "rainArrow 1s ease-out infinite",
-        "enter-left": "enterLeft 0.5s ease-in-out",
-        "right-infinite": "rightInfinite 2s linear infinite",
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+      boxShadow: {
+        input: `0px 2px 3px -1px rgba(0,0,0,0.1), 0px 1px 0px 0px rgba(25,28,33,0.02), 0px 0px 0px 1px rgba(25,28,33,0.08)`,
       },
     },
   },
-  darkMode: "selector",
-  plugins: [],
-};
+  plugins: [require("tailwindcss-animate"), addVariablesForColors],
+} satisfies Config;
+
 export default config;
+
+function addVariablesForColors({ addBase, theme }: any) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+
+  addBase({
+    ":root": newVars,
+  });
+}
